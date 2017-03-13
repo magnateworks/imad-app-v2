3b4a2e71a014a91aecb493486d4f2e3a44722d1b
magnateworks <mangeshmail@gmail.com>
2017-03-14 02:20:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pool').Pool;
+
+var config = {
+    user : 'magnateworks',
+    database : 'magnateworks',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -17,6 +26,18 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('select * from test',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }else{
+         res.send(JSON.stringify(result));
+     }   
+    })
+   
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
