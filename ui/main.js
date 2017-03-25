365eabc7510a68b41eb190f21b07ca295684d092
magnateworks <mangeshmail@gmail.com>
2017-03-25 23:50:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-var counter=0;
+var count=0;
 var span = document.getElementById("counter");
 
 span.onclick = function(){
-    counter=counter+1;
-    span.innerHTML=counert.toString();
+    count=count+1;
+    span.innerHTML=count.toString();
 }
 
