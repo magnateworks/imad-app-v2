909faa83641940640d188f272f6757af5551535e
magnateworks <mangeshmail@gmail.com>
2017-03-25 20:44:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,16 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "New Value!";
+
+var img = document.getElementById("madi");
+
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + "px";
+}
+
+img.OnClick = function() {
+    setInterval(moveRight,50);
+}
