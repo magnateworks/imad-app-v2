87b4b288cdae3a8a6d1d7d05b803be85e54eac32
magnateworks <mangeshmail@gmail.com>
2017-03-26 00:12:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,24 @@ var button = document.getElementById("counter");
 var span = document.getElementById("count");
 
 button.onclick = function(){
-    count=count+1;
-    span.innerHTML=count.toString();
+
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the resopnse and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = count.toString;
+            }
+        }    
+    };
+    
+    //make request
+    request.open('GET', 'http://www.http://magnateworks.imad.hasura-app.io/counter', true);
+    request.send(null);
+
 };
 
