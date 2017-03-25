65a3c10b3f0b1472d447d533e9cf917dda3677b4
magnateworks <mangeshmail@gmail.com>
2017-03-26 00:23:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function(){
     };
     
     //make request
-    request.open('GET', 'magnateworks.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://magnateworks.imad.hasura-app.io/counter', true);
     request.send(null);
 
 };
