fc9afd6d610de4cbd1796e101b7327b80845d5a4
magnateworks <mangeshmail@gmail.com>
2017-03-25 16:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,20 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+app.get('/article-one', function (req, res) {
+  res.send("Article one requested and will be served here.");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("Article two requested and will be served here.");
+});
+
+app.get('/article-three', function (req, res) {
+  res.send("Article three requested and will be served here.");
+});
+
+
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');     
     return hashed.toString('hex');
