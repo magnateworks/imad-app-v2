9821bd427db5bd6fe1e191f73645a069354a2b0e
magnateworks <mangeshmail@gmail.com>
2017-03-25 23:14:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,10 +109,6 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;    
-  res.send(createTemplate(articles[articleName]));
-});
 
 var counter = 0 ;
 
@@ -120,6 +116,13 @@ app.get('/counter',function(req,res){
     counter = counter + 1 ;
     res.send(counter.toString());
 });
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;    
+  res.send(createTemplate(articles[articleName]));
+});
+
+
 
 
 function hash(input,salt){
